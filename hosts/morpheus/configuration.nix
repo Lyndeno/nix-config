b1975bdc9abd8f2dd5f4f3c159645dc4c3b78dcc
Lyndon Sanche <lsanche@lyndeno.ca>
2021-11-30 09:44:10 -0700

    Enable steam

--- hosts/morpheus/configuration.nix
+++ hosts/morpheus/configuration.nix
@@ -17,6 +17,8 @@
   modules.desktop.enable = true;
   modules.pia-vpn.enable = true;
 
+  programs.steam.enable = true;
+
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
