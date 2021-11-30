71df7d2121867976b8b7b27128e2b9d0cbebeadb
Lyndon Sanche <lsanche@lyndeno.ca>
2021-11-30 12:39:49 -0700

    Desktop hardware changes

--- hosts/morpheus/hardware.nix
+++ hosts/morpheus/hardware.nix
@@ -16,10 +16,22 @@ in
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
+
+  hardware.enableRedistributableFirmware = true;
+  hardware.cpu.amd.updateMicrocode = true;
+
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  hardware.opengl.extraPackages = with pkgs; [
+    rocm-opencl-icd
+    rocm-opencl-runtime
+  ];
+
+  hardware.opengl.driSupport = true;
+  hardware.opengl.driSupport32Bit = true;
 
   fileSystems = {
     "/" = rootSubvol "root";
