0fbee570bce6f4443112b56e179292637d1c1e73
Lyndon Sanche <lsanche@lyndeno.ca>
2021-12-08 01:59:38 -0700

    Clean up

--- common.nix
+++ common.nix
@@ -56,14 +56,12 @@
     syntaxHighlighting.enable = true;
   };
 
+  programs.git.enable = true;
+
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
     pueue
-    git
-    gnupg
-    pinentry-curses
     neofetch
     starship
     exa
