dbed6d8e19b1f6285a83d85619f171d60d05fc58
Lyndon Sanche <lsanche@lyndeno.ca>
2022-01-12 11:55:25 -0700

    Switch morpheus to basic filesystem layout

--- hosts/morpheus/hardware.nix
+++ hosts/morpheus/hardware.nix
@@ -2,19 +2,8 @@
 # and may be overwritten by future invocations.  Please make changes
 # to /etc/nixos/configuration.nix instead.
 { config, lib, pkgs, modulesPath, ... }:
-let
-	rootSubvol = subvol:
-	{
-		device = "/dev/disk/by-uuid/545b7cd0-e18b-494c-8d26-1ee1392db5ab";
-		fsType = "btrfs";
-		options = [ "noatime" "compress=zstd" "subvol=${subvol}" ];
-	};
-in
-{
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
 
+{
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
@@ -34,27 +23,18 @@ in
   hardware.opengl.driSupport32Bit = true;
 
   fileSystems = {
-    "/" = rootSubvol "root";
-    "/home" = rootSubvol "home";
-    "/root" = rootSubvol "home/root";
-    "/nix" = rootSubvol "nix";
-    "/var/log" = rootSubvol "log";
-    "/srv/torrents" = rootSubvol "torrents";
-    "/srv/plex" = rootSubvol "plex";
-    "/boot" =
-        { device = "/dev/disk/by-uuid/3F5E-1123";
-          fsType = "vfat";
-        };
-    "/data/mirror" = 
-      {
-        device = "/dev/disk/by-uuid/155d647d-ca6e-4b08-8c88-9cc2a49c9a5d";
-        fsType = "btrfs";
-        options = [ "noatime" "compress=zstd" "subvol=@toplevel" ];
-      };
+	  "/" = {
+      device = "/dev/nixpool/nixroot";
+      fsType = "ext4";
+	  };
+
+	  "/boot" = {
+      device = "/dev/disk/by-label/ESP";
+      fsType = "vfat";
+    };
   };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/4171064c-a4f5-4233-baee-c9de03c2df81"; }
+    [ { device = "/dev/nixpool/nixswap"; }
     ];
-
 }
