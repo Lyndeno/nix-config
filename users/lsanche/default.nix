a1ad397eef047b53486212f1958741f561918f30
Lyndon Sanche <lsanche@lyndeno.ca>
2022-01-13 10:07:09 -0700

    Add some morpheus-specific sway config

--- users/lsanche/default.nix
+++ users/lsanche/default.nix
@@ -49,6 +49,28 @@ in
     #    (lib.removePrefix "$HOME" "${homecfg.programs.zsh.history.path}")
     #  ];
     #};
+    wayland.windowManager.sway.config.output = lib.mkIf (config.networking.hostName == "morpheus" ) {
+      DP-1 = {
+        adaptive_sync = "on";
+        pos = "1920 0";
+        mode = "2560x1440@144.000000Hz";
+      };
+      DP-2 = {
+        pos = "0 300";
+      };
+    };
     home.stateVersion = config.system.stateVersion;
+
+    wayland.windowManager.sway.config.workspaceOutputAssign = lib.mkIf (config.networking.hostName == "morpheus" ) [
+      { workspace = "1"; output = "DP-1"; }
+      { workspace = "2"; output = "DP-1"; }
+      { workspace = "3"; output = "DP-1"; }
+      { workspace = "4"; output = "DP-1"; }
+      { workspace = "5"; output = "DP-1"; }
+      { workspace = "6"; output = "DP-2"; }
+      { workspace = "7"; output = "DP-2"; }
+      { workspace = "8"; output = "DP-2"; }
+      { workspace = "9"; output = "DP-2"; }
+    ];
   };
   }
