5e2097448b6ad3b4321ede07107259688153d3d2
Lyndon Sanche <lsanche@lyndeno.ca>
2022-01-17 18:31:31 -0700

    Reference host-specific hm config as per Lyndeno/nix-home@65b31c9

--- users/lsanche/default.nix
+++ users/lsanche/default.nix
@@ -24,10 +24,15 @@ in
   users.groups = {
     "${myUsername}" = {};
   };
-  home-manager.users."${myUsername}" = { pkgs, ... }: {
+  home-manager.users."${myUsername}" = { pkgs, ... }:
+  let
+    hostConfig = ./home-manager/hosts + "/${config.networking.hostName}";
+  in
+  {
     imports = [
       #<impermanence/home-manager.nix>
       ./home-manager/home.nix
+      hostConfig
     ];
     #home.persistence."/nix/persist${config.users.users.${myUsername}.home}" = let
     #  homecfg = config.home-manager.users."${myUsername}";
@@ -49,28 +54,7 @@ in
     #    (lib.removePrefix "$HOME" "${homecfg.programs.zsh.history.path}")
     #  ];
     #};
-    wayland.windowManager.sway.config.output = lib.mkIf (config.networking.hostName == "morpheus" ) {
-      DP-1 = {
-        adaptive_sync = "on";
-        pos = "1920 0";
-        mode = "2560x1440@144.000000Hz";
-      };
-      DP-2 = {
-        pos = "0 300";
-      };
-    };
     home.stateVersion = config.system.stateVersion;
 
-    wayland.windowManager.sway.config.workspaceOutputAssign = lib.mkIf (config.networking.hostName == "morpheus" ) [
-      { workspace = "1"; output = "DP-1"; }
-      { workspace = "2"; output = "DP-1"; }
-      { workspace = "3"; output = "DP-1"; }
-      { workspace = "4"; output = "DP-1"; }
-      { workspace = "5"; output = "DP-1"; }
-      { workspace = "6"; output = "DP-2"; }
-      { workspace = "7"; output = "DP-2"; }
-      { workspace = "8"; output = "DP-2"; }
-      { workspace = "9"; output = "DP-2"; }
-    ];
   };
   }
