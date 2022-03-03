ed5248585104a8483d0ec3eb2f1de7d0b355c21b
Lyndon Sanche <lsanche@lyndeno.ca>
2022-03-03 00:07:29 -0700

    Make vscode use xorg until fixed

--- users/lsanche/home-manager/home.nix
+++ users/lsanche/home-manager/home.nix
@@ -132,16 +132,16 @@ in
     };
     vscode = {
       enable = true;
-      package = (pkgs.symlinkJoin {
-              name = "vscode";
-              pname = "vscode";
-              paths = [ pkgs.vscode ];
-              buildInputs = [ pkgs.makeWrapper ];
-              postBuild = ''
-                  wrapProgram $out/bin/code \
-                  --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland"
-              '';
-          });
+      #package = (pkgs.symlinkJoin {
+      #        name = "vscode";
+      #        pname = "vscode";
+      #        paths = [ pkgs.vscode ];
+      #        buildInputs = [ pkgs.makeWrapper ];
+      #        postBuild = ''
+      #            wrapProgram $out/bin/code \
+      #            --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland"
+      #        '';
+      #    });
       extensions = (with pkgs.vscode-extensions; [
         vscodevim.vim
         ms-vscode.cpptools
