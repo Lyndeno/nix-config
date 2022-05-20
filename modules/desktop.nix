263a70bac33d9bed027f89bc235c633b3bafb048
Lyndon Sanche <lsanche@lyndeno.ca>
2022-05-20 08:50:31 -0600

    Experimenting with gnome

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -16,23 +16,7 @@ in
   };
 
   config = mkIf cfg.enable {
-    #nixpkgs.overlays = [
-    #  (self: super: {
-    #    plasma5Packages = super.plasma5Packages.overrideScope' ( pself: psuper: {
-
-    #    kwallet = psuper.kwallet.overrideAttrs ( oldAttrs: {
-    #      patches = [ (super.fetchpatch {
-    #          url = "https://invent.kde.org/frameworks/kwallet/-/merge_requests/11.patch";
-    #          sha256 = "sha256-kGWuhOPH+QddsiRaDCfzKgEf++gOYbQHUFBHEzDZXHE=";
-    #        }
-    #        )
-    #      ];
-    #    });
-    #  });
-    #})
-    #];
-
-    programs.gnupg.agent.pinentryFlavor = "qt";
+    programs.gnupg.agent.pinentryFlavor = "gnome3";
 
 
     services = {
@@ -42,18 +26,31 @@ in
             alsa.support32Bit = true;
             pulse.enable = true;
         };
-        gnome.gnome-keyring.enable = true;
+        #gnome.gnome-keyring.enable = true;
         #gvfs.enable = true; # for nautilus
     };
   services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.defaultSession = "plasmawayland";
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  #services.xserver.displayManager.defaultSession = "plasmawayland";
+  services.xserver.desktopManager.gnome.enable = true;
+
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    gnome-music
+    gedit
+    epiphany
+    gnome-characters
+    #tali
+    #iagno
+    #hitori
+    #atomix
+  ]);
 
 
     security = {
         rtkit.enable = true; # Realtime pipewire
-        pam.services.sddm.u2fAuth = false;
+        pam.services.gdm.u2fAuth = false;
         pam.services.sddm.enableGnomeKeyring = true;
     };
 
@@ -67,9 +64,9 @@ in
 
 
     boot.kernelModules = lib.optional cfg.supportDDC "i2c_dev";
-    services.udev.extraRules = lib.optionalString cfg.supportDDC ''
-        KERNEL=="i2c-[0-9]*", TAG+="uaccess"
-    '';
+    #services.udev.extraRules = lib.optionalString cfg.supportDDC ''
+    #    KERNEL=="i2c-[0-9]*", TAG+="uaccess"
+    #'';
 
     xdg.portal = {
         enable = true;
@@ -82,41 +79,19 @@ in
 
     hardware.pulseaudio.enable = false; # use pipewire instead
 
+    programs.kdeconnect = {
+      enable = true;
+      package = pkgs.gnomeExtensions.gsconnect;
+    };
+
     environment.systemPackages = with pkgs; [
         firefox-wayland
         pavucontrol
-        #(symlinkJoin {
-        #    name = "vscode";
-        #    paths = [ vscode ];
-        #    buildInputs = [ makeWrapper ];
-        #    postBuild = ''
-        #        wrapProgram $out/bin/code \
-        #        --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland"
-        #    '';
-        #})
         alacritty
-        gnome.nautilus
         libnotify
-        capitaine-cursors
-        (lib.mkIf cfg.supportDDC ddcutil)
+        #(lib.mkIf cfg.supportDDC ddcutil)
         brightnessctl
-        pulseaudio # for pactl
         xdg-utils
-      ] ++ (with plasma5Packages; [
-        kmail
-        kmail-account-wizard
-        kmailtransport
-        kalendar
-        kaddressbook
-        accounts-qt
-        kdepim-runtime
-        kdepim-addons
-        ark
-        okular
-        filelight
-        partition-manager
-        brave
-        plasma-browser-integration
         ( pkgs. symlinkJoin {
             name = "vscode";
             pname = "vscode";
@@ -127,6 +102,24 @@ in
                 --add-flags "--ozone-platform=wayland --enable-features=WaylandWindowDecorations"
             '';
         })
+        brave
+        gnomeExtensions.appindicator
+        gnome.gnome-tweaks
+      ] ++ (with plasma5Packages; [
+        #kmail
+        #kmail-account-wizard
+        #kmailtransport
+        #kalendar
+        #kaddressbook
+        #accounts-qt
+        #kdepim-runtime
+        #kdepim-addons
+        #ark
+        #okular
+        #filelight
+        #partition-manager
+        #plasma-browser-integration
       ]);
+      services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
   };
 }
