5c6c420ed04e6ee79e7eba908c2ba9c1c35a9df4
Lyndon Sanche <lsanche@lyndeno.ca>
2022-06-10 12:25:41 -0600

    Clean up neo config

--- hosts/neo/configuration.nix
+++ hosts/neo/configuration.nix
@@ -21,62 +21,6 @@
     };
   };
 
-  #networking.nat.enable = true;
-  #networking.nat.internalInterfaces = [ "ve-torrents" ];
-  #networking.nat.externalInterface = "wlp2s0";
-
-  #containers.torrents = {
-  #  enableTun = true;
-  #  autoStart = true;
-  #  privateNetwork = true;
-  #  hostAddress = "192.168.100.2";
-  #  localAddress = "192.168.100.11";
-  #  forwardPorts = [
-  #    {
-  #      containerPort = 9091;
-  #      hostPort = 9091;
-  #      protocol = "tcp";
-  #    }
-  #    {
-  #      containerPort = 9091;
-  #      hostPort = 9091;
-  #      protocol = "udp";
-  #    }
-  #  ];
-  #  config = { config, pkgs, ... }: {
-  #    services.transmission.enable = true;
-  #    services.transmission.openRPCPort = true;
-  #                 
-  #    services.openvpn.servers = {
-  #      pia-vancouver = {
-  #        autoStart = false;
-  #        config = "config /etc/nixos/vpn/pia-vancouver.ovpn";
-  #      };
-  #    };
-  #  };
-  #};
-
-  #specialisation = {
-  #  plexMode = {
-  #    configuration = {
-  #      services.plex = {
-  #        enable = true;
-  #        openFirewall = true;
-  #        group = "media";
-  #      };
-  #    };
-  #  };
-  #};
-
-  #virtualisation = {
-  #  libvirtd = {
-  #    enable = true;
-  #  };
-  #};
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
   networking = {
     useDHCP = false;
     networkmanager.enable = true;
@@ -87,7 +31,6 @@
 
   services = {
     logind.lidSwitch = "suspend-then-hibernate";
-    #tlp.enable = true;
     power-profiles-daemon.enable = true;
   };
   
@@ -99,7 +42,6 @@
 
   environment.systemPackages = with pkgs; [
     libsmbios # For fan control
-    #virt-manager
   ];
 
   system.stateVersion = "21.11"; # Did you read the comment?
