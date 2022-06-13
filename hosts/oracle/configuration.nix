943e3c7bb3c1a35ad934fff1764faadac83eb563
Lyndon Sanche <lsanche@lyndeno.ca>
2022-06-13 09:51:50 -0600

    Auto upgrade oracle

--- hosts/oracle/configuration.nix
+++ hosts/oracle/configuration.nix
@@ -44,6 +44,12 @@
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
 
+  system.autoUpgrade = {
+    enable = true;
+    flake = "github:Lyndeno/nix-config/master";
+    allowReboot = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
