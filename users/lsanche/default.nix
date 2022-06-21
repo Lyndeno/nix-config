da5e28a4469d7e3fe7ab77ebb434b86418c79fcc
Lyndon Sanche <lsanche@lyndeno.ca>
2022-06-21 00:13:11 -0600

    Do not have fzf overwrite terminal bg

--- users/lsanche/default.nix
+++ users/lsanche/default.nix
@@ -38,7 +38,7 @@ in
     # Referenced https://github.com/base16-project/base16-fzf/blob/main/templates/default.mustache
     programs.fzf.enable = true;
     programs.fzf.defaultOptions = with config.scheme.withHashtag; [
-      "--color=bg+:${base01},bg:${base00},spinner:${base0C},hl:${base0D}"
+      "--color=spinner:${base0C},hl:${base0D}"
       "--color=fg:${base04},header:${base0D},info:${base0A},pointer:${base0C}"
       "--color=marker:${base0C},fg+:${base06},prompt:${base0A},hl+:${base0D}"
     ];
