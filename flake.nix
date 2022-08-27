223aec0ac5f1275e8f63601ac82ccbbe4eec865e
Lyndon Sanche <lsanche@lyndeno.ca>
2022-08-27 16:48:49 -0600

    Draft raspPi config

--- flake.nix
+++ flake.nix
@@ -58,6 +58,10 @@
         allowUnfree = true;
       };
     };
+    pkgsPi = import inputs.nixpkgs {
+      system = "aarch64-linux";
+      config.allowUnfree = true;
+    };
 
     lib = inputs.nixpkgs.lib;
 
@@ -153,6 +157,56 @@
           };
         })
       ];
+
+      trinity = lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        pkgs = pkgsPi;
+        system = "aarch64-linux";
+        modules = commonModules ++ [
+          ({config, ...}: {
+            #nixpkgs.crossSystem.system = "aarch64-linux";
+            boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages; # Raspberry pies have a hard time booting on the LTS kernel.
+            boot = {
+              tmpOnTmpfs = true;
+              initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
+              kernelParams = [
+                "8250.nr_uarts=1"
+                "console=ttyAMA0,115200"
+                "console=tty1"
+                "cma=128M"
+              ];
+            };
+
+            boot.loader.raspberryPi = {
+              enable = true;
+              version = 4;
+            };
+            boot.loader.grub.enable = false;
+
+            fileSystems = {
+              "/" = {
+                device = "/dev/disk/by-label/NIXOS_SD";
+                fsType = "ext4";
+              };
+            };
+
+            hardware.enableRedistributableFirmware = true;
+
+            networking = {
+              hostName = "trinity";
+              networkmanager = {
+                enable = true;
+              };
+            };
+            services.openssh = {
+              enable = true;
+              permitRootLogin = "yes";
+            };
+            system.stateVersion = "22.05";
+            users.users."root".initialPassword = "root";
+          })
+        ];
+      };
     };
   };
 }
