da91d647bae24fef0d3a21dfe1d304ddaf922640
Lyndon Sanche <lsanche@lyndeno.ca>
2022-08-27 20:07:41 -0600

    Store user pubkeys in dedicated file

--- users/lsanche/default.nix
+++ users/lsanche/default.nix
@@ -15,10 +15,12 @@ in
       (lib.mkIf config.networking.networkmanager.enable "networkmanager") # Do not add this group if networkmanager is not enabled
       "libvirtd"
     ];
-    openssh.authorizedKeys.keys = [
-      (lib.mkIf (config.networking.hostName == "morpheus") "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEjPlzlPcny6ZKwNzdlzi85lrIhPtdjLDRov29Fbef60" )
-      (lib.mkIf (config.networking.hostName == "neo") "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINtYA9xm9hQtFt5r7WuED1kkmvfezCURg6Tx9Ch1q0Ie" )
-      (lib.mkIf (config.networking.hostName == "oracle") "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8hU9yYrAg76q1zp6rfhOBxSjwSwzQFpJTdBynWSCKA" )
+    openssh.authorizedKeys.keys = let
+      keys = import ./pubkeys.nix;
+    in [
+      (lib.mkIf (config.networking.hostName == "morpheus") keys.morpheus )
+      (lib.mkIf (config.networking.hostName == "neo") keys.neo )
+      (lib.mkIf (config.networking.hostName == "oracle") keys.oracle )
     ];
     shell = pkgs.zsh;
   };
---
+++ users/lsanche/pubkeys.nix
@@ -0,0 +1,5 @@
+{
+  morpheus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEjPlzlPcny6ZKwNzdlzi85lrIhPtdjLDRov29Fbef60";
+  neo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINtYA9xm9hQtFt5r7WuED1kkmvfezCURg6Tx9Ch1q0Ie";
+  oracle = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8hU9yYrAg76q1zp6rfhOBxSjwSwzQFpJTdBynWSCKA";
+}
