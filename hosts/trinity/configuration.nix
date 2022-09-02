c053e99878173313a20d39a00a23fdfd52d9b405
Lyndon Sanche <lsanche@lyndeno.ca>
2022-09-02 00:29:01 +0000

    Implement neo borg repo on trinity

--- hosts/trinity/configuration.nix
+++ hosts/trinity/configuration.nix
@@ -8,6 +8,13 @@
     gtk.enable = false;
   };
 
+  services.borgbackup.repos = {
+    neo = {
+      path = "/data/borg/neo";
+      authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL/jyR1sMTHU3LoSweCtlAQwtaeUJGw/2LmOAKDuEXE3" ];
+    };
+  };
+
   modules.services.nebula = {
     enable = true;
     nodeName = "trinity";
