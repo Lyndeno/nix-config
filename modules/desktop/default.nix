832bf3d227aa4575ecd1d5067ddb03b457284980
Lyndon Sanche <lsanche@lyndeno.ca>
2022-09-14 07:54:02 -0600

    Configure central variable to store wayland information

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -11,6 +11,21 @@ let
       name = "CaskaydiaCove Nerd Font";
     };
   };
+
+  environments = {
+    gnome = {
+      wayland = true;
+    };
+    plasma = {
+      wayland = true;
+    };
+    sway = {
+      wayland = true;
+    };
+    i3 = {
+      wayland = true;
+    };
+  };
 in
 {
   imports = [
@@ -28,7 +43,7 @@ in
         software = {
           backup = mkOption {type = types.bool; default = true; };
         };
-        environment = mkOption { type = types.nullOr (types.enum ["gnome" "plasma" "sway" "i3"]); default = null; };
+        environment = mkOption { type = types.nullOr (types.enum (lib.mapAttrsToList (name: value: name) environments)); default = null; };
         mainResolution = {
           width = mkOption { type = types.int; default = 1920; };
           height = mkOption { type = types.int; default = 1080; };
@@ -83,7 +98,7 @@ in
       vscode
     ];
 
-    environment.sessionVariables.NIXOS_OZONE_WL = if cfg.environment != "i3" then "1" else "";
+    environment.sessionVariables.NIXOS_OZONE_WL = if environments.${cfg.environment}.wayland then "1" else "";
 
     stylix.targets = {
       gnome.enable = false;
