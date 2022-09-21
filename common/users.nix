2a8f6dac06cb637f26d22007cfb7d788ec19c65b
Lyndon Sanche <lsanche@lyndeno.ca>
2022-09-21 14:45:04 -0600

    Further compact user config

--- common/users.nix
+++ common/users.nix
@@ -8,8 +8,28 @@ let
     })
     allUsers
   );
+
+  userConfigs = (map
+    (x: {
+      users.users.${x} = (import ../users/${x}/user.nix { inherit config pkgs lib; username = x; })
+        //
+        {
+          openssh.authorizedKeys.keys = [
+            (lib.mkIf (userKeys.${x} ? ${config.networking.hostName}) userKeys.${x}.${config.networking.hostName} )
+          ];
+        };
+
+      users.groups.${x} = {};
+
+      home-manager.users.${x} = { pkgs, ...}: {
+        imports = [ ../users/${x}/home-manager/home.nix ];
+        home.stateVersion = config.system.stateVersion;
+      };
+    })
+    allUsers
+  );
 in
-{
+lib.mkMerge ( [{
   #warnings = lib.mkIf (!checkKey) [
   #  "User 'lsanche' does not have valid login ssh key for hostname '${config.networking.hostName}'"
   #];
@@ -18,40 +38,6 @@ in
     useUserPackages = true;
   };
   programs.fuse.userAllowOther = true;
+  users.groups.media = {};
 
-  users.users = builtins.listToAttrs (map
-    (x: {
-      name = x;
-      value = 
-        (import ../users/${x}/user.nix { inherit config pkgs lib; username = x; })
-        //
-        {
-          openssh.authorizedKeys.keys = [
-            (lib.mkIf (userKeys.${x} ? ${config.networking.hostName}) userKeys.${x}.${config.networking.hostName} )
-          ];
-        };
-    })
-    allUsers
-  );
-
-  users.groups = builtins.listToAttrs (map
-    (x: {
-      name = x;
-      value = {};
-    })
-    allUsers
-  ) // {
-    media = {};
-  };
-
-  home-manager.users = builtins.listToAttrs (map
-    (x: {
-      name = x;
-      value = { pkgs, ...}: {
-        imports = [ ../users/${x}/home-manager/home.nix ];
-        home.stateVersion = config.system.stateVersion;
-      };
-    })
-    allUsers
-  );
-}
+}] ++ userConfigs)
