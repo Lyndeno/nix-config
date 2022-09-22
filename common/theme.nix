5549a7b51ca6fb715d360b83fea1f3a1d8c1c52e
Lyndon Sanche <lsanche@lyndeno.ca>
2022-09-22 09:51:43 -0600

    Clean up theme config a bit

--- common/theme.nix
+++ common/theme.nix
@@ -2,23 +2,25 @@
 let
   base16Scheme = "${inputs.base16-schemes}/gruvbox-dark-hard.yaml";
 in {
-  stylix.image = "${inputs.wallpapers}/starry.jpg";
-  stylix.base16Scheme = base16Scheme;
-  stylix.targets.swaylock.useImage = false;
-  stylix.fonts = let
-    cascadia = (pkgs.nerdfonts.override { fonts = [ "CascadiaCode" ]; });
-  in {
-    serif = {
-      package = cascadia;
-      name = "CaskaydiaCove Nerd Font";
-    };
-    sansSerif = {
-      package = cascadia;
-      name = "CaskaydiaCove Nerd Font";
-    };
-    monospace = {
-      package = cascadia;
-      name = "CaskaydiaCove Nerd Font Mono";
-    };
+    stylix = {
+      image = "${inputs.wallpapers}/starry.jpg";
+      base16Scheme = base16Scheme;
+      targets.swaylock.useImage = false;
+      fonts = let
+        cascadia = (pkgs.nerdfonts.override { fonts = [ "CascadiaCode" ]; });
+      in {
+        serif = {
+          package = cascadia;
+          name = "CaskaydiaCove Nerd Font";
+        };
+        sansSerif = {
+          package = cascadia;
+          name = "CaskaydiaCove Nerd Font";
+        };
+        monospace = {
+          package = cascadia;
+          name = "CaskaydiaCove Nerd Font Mono";
+        };
+      };
   };
 }
