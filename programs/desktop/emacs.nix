74db589594d22378ea1c271d4f358a70b9b050ea
Lyndon Sanche <lsanche@lyndeno.ca>
2022-09-28 12:47:02 -0600

    Use emacs gtk

--- programs/desktop/emacs.nix
+++ programs/desktop/emacs.nix
@@ -2,6 +2,7 @@
 {
   home-manager.users.lsanche.programs.emacs = {
     enable = true;
+    package = pkgs.emacs-gtk;
     extraPackages = epkgs: [ epkgs.evil epkgs.base16-theme];
     extraConfig = ''
       (require 'evil)
