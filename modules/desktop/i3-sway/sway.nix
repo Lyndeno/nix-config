6811befab2f15c7f41ffdc91e85dd2325d3a6feb
Lyndon Sanche <lsanche@lyndeno.ca>
2022-10-21 12:44:34 -0600

    Center bemenu

--- modules/desktop/i3-sway/sway.nix
+++ modules/desktop/i3-sway/sway.nix
@@ -88,7 +88,7 @@ in
       commands = {
         lock = "${pkgs.swaylock}/bin/swaylock -f";
         terminal = "${pkgs.alacritty}/bin/alacritty";
-        menu = "${pkgs.bemenu}/bin/bemenu-run ${config.home-manager.users.lsanche.home.sessionVariables.BEMENU_OPTS} -b -H 25";
+        menu = "${pkgs.bemenu}/bin/bemenu-run ${config.home-manager.users.lsanche.home.sessionVariables.BEMENU_OPTS} -b -H 25 -c -l 10 -W 0.3";
       };
     in rec {
 
