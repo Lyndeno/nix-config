06484fbae845451286134ebc8b283a710b0ddd26
Lyndon Sanche <lsanche@lyndeno.ca>
2022-11-18 19:58:16 -0700

    Add gmail config

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -121,11 +121,29 @@ in
         sidebar.enable = true;
         vimKeys = true;
       };
+      programs.astroid = {
+        enable = true;
+      };
+      programs.lieer.enable = true;
+      programs.notmuch.enable = true;
 
       accounts.email = {
         maildirBasePath = ".Maildir";
-        accounts.fastmail = {
+        accounts.gmail = {
           primary = true;
+
+          userName = "lyndeno@gmail.com";
+          address = "lyndeno@gmail.com";
+          realName = "Lyndon Sanche";
+          flavor = "gmail.com";
+
+          lieer.enable = true;
+          notmuch.enable = true;
+          astroid.enable = true;
+          astroid.sendMailCommand = "";
+        };
+        accounts.fastmail = {
+          #primary = true;
           userName = "lsanche@fastmail.com";
           address = "lsanche@fastmail.com";
           realName = "Lyndon Sanche";
