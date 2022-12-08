7f87a4e2b70df0a0b4c33e376b780bdf88738ab2
Lyndon Sanche <lsanche@lyndeno.ca>
2022-12-08 00:02:28 -0700

    Enable mangohud

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -111,6 +111,25 @@ in
 
     home-manager.users.lsanche = {
 
+      programs.mangohud = {
+        enable = true;
+        settings = {
+          cpu_stats = true;
+          gpu_stats = true;
+          cpu_mhz = true;
+          cpu_temp = true;
+          gpu_temp = true;
+          vulkan_driver = true;
+          ram = true;
+          vram = true;
+          position = "bottom-left";
+          frame_timing = false;
+          cpu_power = true;
+          gpu_power = true;
+          arch = true;
+        };
+      };
+
       ### EMAIL ###
       services.imapnotify.enable = true;
       programs.mbsync.enable = true;
