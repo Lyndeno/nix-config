329f92d092de0748ac6ab454d10f56dd994da7a1
Lyndon Sanche <lsanche@lyndeno.ca>
2022-12-20 10:11:58 -0700

    neo: Automatic ZFS scrub and ZED email notifications

--- hosts/neo/hardware.nix
+++ hosts/neo/hardware.nix
@@ -26,6 +26,45 @@
   networking.hostId = "54c50fe2";
   boot.zfs.allowHibernation = true;
 
+  age.secrets.gmail_pass.file = ../../secrets/email_pass.age;
+  programs.msmtp = {
+    enable = true;
+    setSendmail = true;
+    defaults = {
+      aliases = "/etc/aliases";
+      port = 465;
+      tls_trust_file = "/etc/ssl/certs/ca-certificates.crt";
+      tls = "on";
+      auth = "login";
+      tls_starttls = "off";
+    };
+    accounts = {
+      default = {
+        host = "smtp.gmail.com";
+        passwordeval = "${pkgs.busybox}/bin/cat ${config.age.secrets.gmail_pass.path}";
+        user = "lyndeno@gmail.com";
+        from = "neo@lyndeno.ca";
+      };
+    };
+  };
+
+  services.zfs.trim.enable = true;
+  services.zfs.autoScrub.enable = true;
+
+  services.zfs.zed.settings = {
+    ZED_DEBUG_LOG = "/tmp/zed.debug.log";
+    ZED_EMAIL_ADDR = [ "lsanche@lyndeno.ca" ];
+    ZED_EMAIL_PROG = "${pkgs.msmtp}/bin/msmtp";
+    ZED_EMAIL_OPTS = "@ADDRESS@";
+
+    ZED_NOTIFY_INTERVAL_SECS = 3600;
+    ZED_NOTIFY_VERBOSE = true;
+
+    ZED_USE_ENCLOSURE_LEDS = true;
+    ZED_SCRUB_AFTER_RESILVER = true;
+  };
+
+  services.zfs.zed.enableMail = false;
   boot.initrd.luks.devices = {
     "nixboot" = {
       device = "/dev/disk/by-uuid/a29e226a-810e-48fa-932e-03569767185e";
