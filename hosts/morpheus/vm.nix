ca4a748cb2672e0020f7483389a72ffc383d605e
Lyndon Sanche <lsanche@lyndeno.ca>
2022-12-22 21:23:36 -0700

    morpheus: Fix secure boot for libvirt

--- hosts/morpheus/vm.nix
+++ hosts/morpheus/vm.nix
@@ -8,7 +8,7 @@
         package = pkgs.qemu_kvm;
         ovmf = {
           enable = true;
-          packages = [ pkgs.OVMFFull ];
+          packages = [ pkgs.OVMFFull.fd ];
         };
         swtpm.enable = true;
       };
@@ -17,6 +17,11 @@
 
   programs.dconf.enable = lib.mkDefault true;
   environment.systemPackages = with pkgs; [ virt-manager ];
+  #environment.etc = {
+  #  "ovmf/edk2-x86_64-secure-code.fd" = {
+  #    source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
+  #  };
+  #};
 
   boot.kernelParams = [ "iommu=pt" ];
 }
