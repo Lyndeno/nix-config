aef3984e8dd80f9beb6dde2c4ad355fbfa4d3db7
Lyndon Sanche <lsanche@lyndeno.ca>
2023-01-10 12:39:09 -0700

    New implementation of sf-pro font

--- common/theme.nix
+++ common/theme.nix
@@ -1,7 +1,31 @@
 {pkgs, lib, inputs, ...}:
 let
   base16Scheme = "${inputs.base16-schemes}/atelier-dune.yaml";
+  sf-pro = pkgs.stdenv.mkDerivation rec {
+    name = "sf-pro";
+    version = "0.0.1";
+
+    src = pkgs.fetchurl {
+      url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
+      sha256 = "sha256-g/eQoYqTzZwrXvQYnGzDFBEpKAPC8wHlUw3NlrBabHw=";
+    };
+
+    buildInputs = [ pkgs.undmg pkgs.p7zip ];
+    buildPhases = [ "unpackPhase" "installPhase" ];
+    setSourceRoot = "sourceRoot=`pwd`";
+
+    installPhase = ''
+      7z x 'SF Pro Fonts.pkg'
+      7z x 'Payload~'
+      mkdir -p $out/share/fonts
+      mkdir -p $out/share/fonts/opentype
+      mkdir -p $out/share/fonts/truetype
+      mv Library/Fonts/*.otf $out/share/fonts/opentype
+      mv Library/Fonts/*.ttf $out/share/fonts/truetype
+    '';
+  };
 in {
+    fonts.fonts = [ sf-pro ];
     stylix = {
       image = "${inputs.wallpapers}/starry.jpg";
       base16Scheme = base16Scheme;
