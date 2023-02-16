2f50f0952d1abf9f21c5bb3759eadd2e317a6681
Lyndon Sanche <lsanche@lyndeno.ca>
2023-02-16 14:58:18 -0700

    oracle: Set up ipv6 address

--- hosts/oracle/configuration.nix
+++ hosts/oracle/configuration.nix
@@ -36,6 +36,13 @@
     '';
   };
 
+  networking.interfaces.enp1s0.ipv6.addresses = [
+    {
+      address = "2001:19f0:8001:b22:5400:03ff:feb9:980c";
+      prefixLength = 64;
+    }
+  ];
+
   users.users.brandt = {
     isNormalUser = true;
     description = "Brandt Sanche";
