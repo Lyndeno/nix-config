62ca3e813c0a48a435e1a06cb6c80f4cbca286e2
Lyndon Sanche <lsanche@lyndeno.ca>
2023-02-27 22:37:55 -0700

    Disable asteroid email

--- desktop/programs/default.nix
+++ desktop/programs/default.nix
@@ -5,7 +5,7 @@
     #(import ./emacs.nix { inherit config pkgs inputs; })
     (import ./browser.nix { inherit config lib pkgs inputs; })
     (import ./mangohud.nix { inherit config lib pkgs inputs; })
-    (import ./email.nix { inherit config lib pkgs inputs; })
+    #(import ./email.nix { inherit config lib pkgs inputs; })
     (import ./gtk.nix { inherit config lib pkgs inputs; })
     (import ./firefox.nix { inherit config lib pkgs inputs; })
     (import ./misc.nix { inherit config lib pkgs inputs; })
