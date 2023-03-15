05fe27f8f0e4ef32576aec729827333bdbd73ef7
Lyndon Sanche <lsanche@lyndeno.ca>
2023-03-15 09:43:49 -0600

    oracle: Forward port 25565 to morpheus

--- hosts/oracle/configuration.nix
+++ hosts/oracle/configuration.nix
@@ -29,11 +29,19 @@
       iptables -t nat -A PREROUTING -p tcp -i enp1s0 --dport 32400 -j DNAT --to-destination 100.127.0.11:32400
       iptables -I FORWARD -p tcp -d 100.127.0.11 --dport 32400 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
       iptables -I POSTROUTING -t nat -p tcp -d 100.127.0.11 --dport 32400 -j MASQUERADE
+
+      iptables -t nat -A PREROUTING -p tcp -i enp1s0 --dport 25565 -j DNAT --to-destination 100.127.0.11:25565
+      iptables -I FORWARD -p tcp -d 100.127.0.11 --dport 25565 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+      iptables -I POSTROUTING -t nat -p tcp -d 100.127.0.11 --dport 25565 -j MASQUERADE
     '';
     extraStopCommands = ''
       iptables -t nat -D PREROUTING -p tcp --dport 32400 -j DNAT --to-destination 100.127.0.11:32400 || true
       iptables -D FORWARD -p tcp -d 100.127.0.11 --dport 32400 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
       iptables -D POSTROUTING -t nat -p tcp -d 100.127.0.11 --dport 32400 -j MASQUERADE
+
+      iptables -t nat -D PREROUTING -p tcp --dport 25565 -j DNAT --to-destination 100.127.0.11:25565 || true
+      iptables -D FORWARD -p tcp -d 100.127.0.11 --dport 25565 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+      iptables -D POSTROUTING -t nat -p tcp -d 100.127.0.11 --dport 25565 -j MASQUERADE
     '';
   };
 
