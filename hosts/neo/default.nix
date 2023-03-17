a2685558dcfeec2bb6ab85fe2f04837e2321a031
Lyndon Sanche <lsanche@lyndeno.ca>
2023-03-17 10:36:38 -0600

    neo/nvidia: use bumblebee

--- hosts/neo/default.nix
+++ hosts/neo/default.nix
@@ -13,19 +13,20 @@ with inputs.nixos-hardware.nixosModules;
             [
               ./configuration.nix
               ./hardware.nix
-              inputs.nixos-hardware.nixosModules.dell-xps-15-9560-nvidia
+              inputs.nixos-hardware.nixosModules.dell-xps-15-9560
               inputs.nixos-hardware.nixosModules.common-cpu-intel-kaby-lake
             ]
             ++ commonModules;
 
-          ls.desktop.environment = lib.mkForce "i3";
-          hardware.nvidia = {
-            prime = {
-              offload.enable = false;
-              sync.enable = true;
-            };
-            modesetting.enable = true;
-          };
+          services.xserver.displayManager.gdm.wayland = false;
+          ls.desktop.environment = lib.mkForce "gnome";
+          #hardware.nvidia = {
+          #  prime = {
+          #    offload.enable = false;
+          #    sync.enable = true;
+          #  };
+          #  modesetting.enable = true;
+          #};
         };
       };
     })
