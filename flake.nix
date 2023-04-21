e72202f8ffb61f24bc4f886c6c267f1425036e77
Lyndon Sanche <lsanche@lyndeno.ca>
2023-04-21 13:29:33 -0600

    flake: Format

--- flake.nix
+++ flake.nix
@@ -67,10 +67,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-   disko = {
-	url = "github:nix-community/disko";
-	inputs.nixpkgs.follows = "nixpkgs";
-   };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
