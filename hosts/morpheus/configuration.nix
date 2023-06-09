9918dc3cd613c6098ad8f550faa125f19f0c9087
Lyndon Sanche <lsanche@lyndeno.ca>
2023-06-09 13:58:10 -0600

    morpheus: Fix backup job

--- hosts/morpheus/configuration.nix
+++ hosts/morpheus/configuration.nix
@@ -153,16 +153,16 @@
         mode = "repokey-blake2";
         passCommand = "cat ${pass_borgbase.path}";
       };
-      preHook = ''
-        echo "save-off" > /run/minecraft-server.stdin
-        echo "say Saving" > /run/minecraft-server.stdin
-        echo "save-all" > /run/minecraft-server.stdin
-        sleep 5
-        echo "say Backing Up" > /run/minecraft-server.stdin
-      '';
-      postHook = ''
-        echo "save-on" > /run/minecraft-server.stdin
-      '';
+      #preHook = ''
+      #  echo "save-off" > /run/minecraft-server.stdin
+      #  echo "say Saving" > /run/minecraft-server.stdin
+      #  echo "save-all" > /run/minecraft-server.stdin
+      #  sleep 5
+      #  echo "say Backing Up" > /run/minecraft-server.stdin
+      #'';
+      #postHook = ''
+      #  echo "save-on" > /run/minecraft-server.stdin
+      #'';
       environment.BORG_RSH = "ssh -i ${id_borgbase.path}";
       compression = "auto,zstd,10";
       startAt = "hourly";
