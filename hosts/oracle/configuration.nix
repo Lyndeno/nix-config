b574a442f5166fd730630dcfc2bfa53f67d8d036
Lyndon Sanche <lsanche@lyndeno.ca>
2023-06-14 19:55:34 -0600

    oracle: Change morpheus ip

--- hosts/oracle/configuration.nix
+++ hosts/oracle/configuration.nix
@@ -18,24 +18,26 @@ _: {
     "net.ipv4.ip_forward" = 1;
     "net.ipv6.conf.all.forwarding" = 1;
   };
-  networking.firewall = {
+  networking.firewall = let
+    morpheusIp = "100.116.158.45";
+  in {
     extraCommands = ''
-      iptables -t nat -A PREROUTING -p tcp -i enp1s0 --dport 32400 -j DNAT --to-destination 100.127.0.11:32400
-      iptables -I FORWARD -p tcp -d 100.127.0.11 --dport 32400 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-      iptables -I POSTROUTING -t nat -p tcp -d 100.127.0.11 --dport 32400 -j MASQUERADE
+      iptables -t nat -A PREROUTING -p tcp -i enp1s0 --dport 32400 -j DNAT --to-destination ${morpheusIp}:32400
+      iptables -I FORWARD -p tcp -d ${morpheusIp} --dport 32400 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+      iptables -I POSTROUTING -t nat -p tcp -d ${morpheusIp} --dport 32400 -j MASQUERADE
 
-      iptables -t nat -A PREROUTING -p tcp -i enp1s0 --dport 25565 -j DNAT --to-destination 100.127.0.11:25565
-      iptables -I FORWARD -p tcp -d 100.127.0.11 --dport 25565 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-      iptables -I POSTROUTING -t nat -p tcp -d 100.127.0.11 --dport 25565 -j MASQUERADE
+      iptables -t nat -A PREROUTING -p tcp -i enp1s0 --dport 25565 -j DNAT --to-destination ${morpheusIp}:25565
+      iptables -I FORWARD -p tcp -d ${morpheusIp} --dport 25565 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+      iptables -I POSTROUTING -t nat -p tcp -d ${morpheusIp} --dport 25565 -j MASQUERADE
     '';
     extraStopCommands = ''
-      iptables -t nat -D PREROUTING -p tcp --dport 32400 -j DNAT --to-destination 100.127.0.11:32400 || true
-      iptables -D FORWARD -p tcp -d 100.127.0.11 --dport 32400 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-      iptables -D POSTROUTING -t nat -p tcp -d 100.127.0.11 --dport 32400 -j MASQUERADE
+      iptables -t nat -D PREROUTING -p tcp --dport 32400 -j DNAT --to-destination ${morpheusIp}:32400 || true
+      iptables -D FORWARD -p tcp -d ${morpheusIp} --dport 32400 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+      iptables -D POSTROUTING -t nat -p tcp -d ${morpheusIp} --dport 32400 -j MASQUERADE
 
-      iptables -t nat -D PREROUTING -p tcp --dport 25565 -j DNAT --to-destination 100.127.0.11:25565 || true
-      iptables -D FORWARD -p tcp -d 100.127.0.11 --dport 25565 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-      iptables -D POSTROUTING -t nat -p tcp -d 100.127.0.11 --dport 25565 -j MASQUERADE
+      iptables -t nat -D PREROUTING -p tcp --dport 25565 -j DNAT --to-destination ${morpheusIp}:25565 || true
+      iptables -D FORWARD -p tcp -d ${morpheusIp} --dport 25565 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+      iptables -D POSTROUTING -t nat -p tcp -d ${morpheusIp} --dport 25565 -j MASQUERADE
     '';
   };
 
