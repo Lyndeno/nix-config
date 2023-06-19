f2b6710d83442762f3740e0ae9530d77930f79b2
Lyndon Sanche <lsanche@lyndeno.ca>
2023-06-19 15:53:39 -0600

    morpheus: Migrate kernel modules and params

--- hosts/morpheus/cfg/boot/default.nix
+++ hosts/morpheus/cfg/boot/default.nix
@@ -1,3 +1,10 @@
 {
   consoleLogLevel = 3;
+  kernelModules = ["kvm-amd" "jc42" "nct6775" "ddcci"];
+  kernelParams = [
+    "quiet"
+    "systemd.show_status=auto"
+    "rd.udev.log_level=3"
+    "iommu=pt"
+  ];
 }
--- hosts/morpheus/hardware.nix
+++ hosts/morpheus/hardware.nix
@@ -1,11 +1,4 @@
 {config, ...}: {
-  boot.kernelModules = ["kvm-amd" "jc42" "nct6775" "ddcci"];
-  boot.kernelParams = [
-    "quiet"
-    "systemd.show_status=auto"
-    "rd.udev.log_level=3"
-    "iommu=pt"
-  ];
   boot.extraModulePackages = [config.boot.kernelPackages.ddcci-driver];
 
   boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
