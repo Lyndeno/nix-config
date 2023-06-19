986894448849d6cbe4bff38330821edc5448bd4d
Lyndon Sanche <lsanche@lyndeno.ca>
2023-06-19 15:43:56 -0600

    morpheus: Migrate powermanagement

--- hosts/morpheus/hardware.nix
+++ hosts/morpheus/hardware.nix
@@ -1,8 +1,4 @@
-{
-  config,
-  lib,
-  ...
-}: {
+{config, ...}: {
   boot.initrd.availableKernelModules = ["nvme" "mpt3sas" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
   boot.kernelModules = ["kvm-amd" "jc42" "nct6775" "ddcci"];
   boot.kernelParams = [
@@ -15,7 +11,6 @@
   boot.extraModulePackages = [config.boot.kernelPackages.ddcci-driver];
 
   hardware.enableRedistributableFirmware = true;
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 
   boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
 }
