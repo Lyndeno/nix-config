f6c0464bc7005ddbe6c2fbfcf5bb892d1b6250e9
Lyndon Sanche <lsanche@lyndeno.ca>
2023-06-19 15:47:44 -0600

    morpheus: Migrate available kernel modules

--- hosts/morpheus/cfg/boot/initrd.nix
+++ hosts/morpheus/cfg/boot/initrd.nix
@@ -2,4 +2,6 @@
   systemd.enable = true;
 
   luks.devices."cryptroot".device = "/dev/disk/by-label/nixcrypt";
+
+  availableKernelModules = ["nvme" "mpt3sas" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
 }
--- hosts/morpheus/hardware.nix
+++ hosts/morpheus/hardware.nix
@@ -1,5 +1,4 @@
 {config, ...}: {
-  boot.initrd.availableKernelModules = ["nvme" "mpt3sas" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
   boot.kernelModules = ["kvm-amd" "jc42" "nct6775" "ddcci"];
   boot.kernelParams = [
     "quiet"
