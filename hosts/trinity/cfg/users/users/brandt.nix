ea3eefd5ce5cf5a4fe038cb63e0db5853716ff1e
Lyndon Sanche <lsanche@lyndeno.ca>
2023-06-20 10:38:27 -0600

    users: Migrate rest of users

---
+++ hosts/trinity/cfg/users/users/brandt.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  config,
+  lib,
+}: let
+  pubKeys = import ../../../../../home/brandt/pubKeys.nix;
+in {
+  isNormalUser = true;
+  description = "Brandt Sanche";
+  extraGroups = [
+    "wheel"
+  ];
+  shell = pkgs.zsh;
+  openssh.authorizedKeys.keys = [
+    (lib.mkIf (pubKeys ? ${config.networking.hostName}) pubKeys.${config.networking.hostName})
+  ];
+}
