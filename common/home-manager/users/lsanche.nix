d9da5386dcc70cfe46e0191c983ae8dfdbebd406
Lyndon Sanche <lsanche@lyndeno.ca>
2023-06-21 12:45:13 -0600

    lsanche: Disable home-manager on non-desktop systems

--- common/home-manager/users/lsanche.nix
+++ common/home-manager/users/lsanche.nix
@@ -5,9 +5,9 @@
   inputs,
   lsLib,
 }:
-import ../../../home/lsanche/home.nix {
+lib.mkIf config.mods.desktop.enable (import ../../../home/lsanche/home.nix {
   config = config.home-manager.users.lsanche;
   inherit pkgs lib inputs lsLib;
   isDesktop = config.mods.desktop.enable;
   inherit (config.system) stateVersion;
-}
+})
