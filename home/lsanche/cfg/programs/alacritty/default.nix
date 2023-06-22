0f319fbbaea56b90aefc806df53d317a8181996d
Lyndon Sanche <lsanche@lyndeno.ca>
2023-06-21 21:18:12 -0600

    lsanche: Migrate alacritty

---
+++ home/lsanche/cfg/programs/alacritty/default.nix
@@ -0,0 +1,14 @@
+{isDesktop}: {
+  enable = isDesktop;
+  # colours and font managed by stylix
+  settings = {
+    window = {
+      padding = {
+        x = 12;
+        y = 12;
+      };
+      dynamic_padding = true;
+    };
+    mouse.hide_when_typing = true;
+  };
+}
