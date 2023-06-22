d098827a654bc29ab9e522f70230ddc5fde6f255
Lyndon Sanche <lsanche@lyndeno.ca>
2023-06-21 21:37:58 -0600

    lsanche: Remove unused args

--- common/home-manager/users/lsanche.nix
+++ common/home-manager/users/lsanche.nix
@@ -1,13 +1,9 @@
 {
   config,
-  pkgs,
-  lib,
   inputs,
-  lsLib,
 }:
 import ../../../home/lsanche/home.nix {
-  config = config.home-manager.users.lsanche;
   isDesktop = config.mods.desktop.enable;
-  inherit pkgs lib inputs lsLib;
+  inherit inputs;
   inherit (config.system) stateVersion;
 }
