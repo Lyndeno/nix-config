41100204c0d29804aef7588d14e70d702c9ff2c8
Lyndon Sanche <lsanche@lyndeno.ca>
2023-06-27 11:52:36 -0600

    common/nix: Add lyndeno substituter

--- common/nix.nix
+++ common/nix.nix
@@ -12,8 +12,10 @@
   };
   settings.substituters = [
     "https://nix-community.cachix.org"
+    "https://lyndeno.cachix.org"
   ];
   settings.trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "lyndeno.cachix.org-1:SgWoC3FEiNlMvHkmGZfPtlBH6hWewaHBB1Y3ng5mMuw="
   ];
 }
