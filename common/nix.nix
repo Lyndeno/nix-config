3810c39ef49d2c91d339f5e89410fc6c848f694e
Lyndon Sanche <lsanche@lyndeno.ca>
2023-06-27 11:13:37 -0600

    common/nix: Add nix-community cachix

--- common/nix.nix
+++ common/nix.nix
@@ -10,4 +10,10 @@
     dates = "weekly";
     options = "--delete-older-than 60d";
   };
+  settings.substituters = [
+    "https://nix-community.cachix.org"
+  ];
+  settings.trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
 }
