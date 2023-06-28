c5b751c6193bedbdeba6a21608be2c7a6c670690
Lyndon Sanche <lsanche@lyndeno.ca>
2023-06-28 11:51:07 -0600

    actions/common: Do not autobuild, remove extra subs

--- common/nix.nix
+++ common/nix.nix
@@ -10,12 +10,4 @@
     dates = "weekly";
     options = "--delete-older-than 60d";
   };
-  settings.substituters = [
-    "https://nix-community.cachix.org"
-    "https://lyndeno.cachix.org"
-  ];
-  settings.trusted-public-keys = [
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "lyndeno.cachix.org-1:SgWoC3FEiNlMvHkmGZfPtlBH6hWewaHBB1Y3ng5mMuw="
-  ];
 }
