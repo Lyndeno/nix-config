adf866a9cfc19b9f842353ade8fd4967de686921
Lyndon Sanche <lsanche@lyndeno.ca>
2023-08-25 12:31:15 -0600

    morpheus: Fix governor on <6.3 kernel

--- hosts/morpheus/powerManagement.nix
+++ hosts/morpheus/powerManagement.nix
@@ -1,3 +1,10 @@
-{lib}: {
-  cpuFreqGovernor = lib.mkDefault "powersave";
+{
+  lib,
+  config,
+}: {
+  cpuFreqGovernor = builtins.trace "Change governor back to only powersave once kernel is back to 6.3" (lib.mkDefault (
+    if (builtins.elem "amd_pstate=active" config.boot.kernelParams)
+    then "powersave"
+    else "ondemand"
+  ));
 }
