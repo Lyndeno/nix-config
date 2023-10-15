5f711a679575495e6bd4c8fff486da76ea51763a
Lyndon Sanche <lsanche@lyndeno.ca>
2023-10-15 00:27:56 -0600

    home/lsanche: Fix bat manpage formatting

--- home/lsanche/home/sessionVariables.nix
+++ home/lsanche/home/sessionVariables.nix
@@ -7,6 +7,7 @@ lib.mkMerge [
   {
     # TODO: For some reason bat cannot theme man pages with custom themes, so we unset here
     MANPAGER = "sh -c '${pkgs.util-linux}/bin/col -bx | BAT_THEME= ${pkgs.bat}/bin/bat -l man -p'";
+    MANROFFOPT = "-c";
   }
   (lib.mkIf isDesktop {
     BROWSER = "firefox";
