9ea20e599745a9ef2e17844cccf454e9ad84821f
Lyndon Sanche <lsanche@lyndeno.ca>
2023-11-30 12:38:52 -0700

    hosts/morpheus: Migrate from deprecated postgresql options

--- hosts/morpheus/services/postgresql.nix
+++ hosts/morpheus/services/postgresql.nix
@@ -2,15 +2,10 @@
   enable = true;
   ensureDatabases = ["paperless"];
   package = pkgs.postgresql_16;
-  initialScript = pkgs.writeText "init-sql-script" ''
-    ALTER DATABASE paperless OWNER TO paperless;
-  '';
   ensureUsers = [
     {
       name = "paperless";
-      ensurePermissions = {
-        "DATABASE paperless" = "ALL PRIVILEGES";
-      };
+      ensureDBOwnership = true;
     }
   ];
 }
