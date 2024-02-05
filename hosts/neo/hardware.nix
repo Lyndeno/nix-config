18272dff180251ea9d9ba557363cc9ea9f9994d3
Lyndon Sanche <lsanche@lyndeno.ca>
2024-02-05 12:52:49 -0700

    host/neo: Enable opencl for intel gpu

--- hosts/neo/hardware.nix
+++ hosts/neo/hardware.nix
@@ -1,4 +1,8 @@
 {
+  pkgs,
+  lib,
+}: {
   enableRedistributableFirmware = true;
   bluetooth.enable = true;
+  opengl.extraPackages = lib.warn "Upstream this to nixos-hardware" [pkgs.intel-compute-runtime];
 }
