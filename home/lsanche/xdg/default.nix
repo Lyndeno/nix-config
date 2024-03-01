c0c0a4a4ca8eb2aa07411b041076fe042acc1e14
Lyndon Sanche <lsanche@lyndeno.ca>
2024-02-29 20:26:23 -0700

    Fix an oops

--- home/lsanche/xdg/default.nix
+++ home/lsanche/xdg/default.nix
@@ -1,7 +1,10 @@
-{}: {
+{
+  lib,
+  pkgs,
+}: {
   enable = true;
-  #configFile."autostart/gnome-keyring-ssh.desktop".text = ''
-  #  ${lib.fileContents "${pkgs.gnome.gnome-keyring}/etc/xdg/autostart/gnome-keyring-ssh.desktop"}
-  #  Hidden=true
-  #'';
+  configFile."autostart/gnome-keyring-ssh.desktop".text = ''
+    ${lib.fileContents "${pkgs.gnome.gnome-keyring}/etc/xdg/autostart/gnome-keyring-ssh.desktop"}
+    Hidden=true
+  '';
 }
