578e088a51af71b1303c5ff0e423dc451a17c2c2
Lyndon Sanche <lsanche@lyndeno.ca>
2024-03-05 12:20:05 -0700

    morpheus: Remove note about old kernel

--- hosts/morpheus/powerManagement.nix
+++ hosts/morpheus/powerManagement.nix
@@ -1,10 +1,3 @@
 {
-  lib,
-  config,
-}: {
-  cpuFreqGovernor = lib.warn "Change governor back to only powersave once kernel is back to 6.3" (lib.mkDefault (
-    if (builtins.elem "amd_pstate=active" config.boot.kernelParams)
-    then "powersave"
-    else "ondemand"
-  ));
+  cpuFreqGovernor = "powersave";
 }
