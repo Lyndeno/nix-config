927a413c90ff71a108797af67863758e08f34503
Lyndon Sanche <lsanche@lyndeno.ca>
2024-03-09 19:08:47 -0700

    Make statix more restrictive

--- hosts/morpheus/services/smartd.nix
+++ hosts/morpheus/services/smartd.nix
@@ -1,6 +1,8 @@
 {
   enable = true;
-  notifications.mail.sender = "morpheus@lyndeno.ca";
-  notifications.mail.recipient = "lsanche@lyndeno.ca";
-  notifications.mail.enable = true;
+  notifications.mail = {
+    sender = "morpheus@lyndeno.ca";
+    recipient = "lsanche@lyndeno.ca";
+    enable = true;
+  };
 }
