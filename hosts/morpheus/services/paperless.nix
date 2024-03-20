fd4e30a0faa06d597d59045a9d0a2b2eb3dd778c
Lyndon Sanche <lsanche@lyndeno.ca>
2024-03-20 14:58:53 -0600

    host/morpheus: Fix paperless classifier NixOS/nixpkgs#240591

--- hosts/morpheus/services/paperless.nix
+++ hosts/morpheus/services/paperless.nix
@@ -1,7 +1,11 @@
 {
+  pkgs,
+  lib,
+}: {
   enable = true;
   address = "0.0.0.0";
   settings = {
     PAPERLESS_DBHOST = "/run/postgresql";
+    LD_LIBRARY_PATH = "${lib.getLib pkgs.mkl}/lib";
   };
 }
