8f3aca436d8e41452eb6db99423bc183f9215190
Lyndon Sanche <lsanche@lyndeno.ca>
2024-03-21 16:09:47 -0600

    host/morpheus: Disable workqueue for encrypted nvme

--- hosts/morpheus/boot/initrd.nix
+++ hosts/morpheus/boot/initrd.nix
@@ -1,7 +1,10 @@
 {
   systemd.enable = true;
 
-  luks.devices."cryptroot".device = "/dev/disk/by-label/nixcrypt";
+  luks.devices."cryptroot" = {
+    device = "/dev/disk/by-label/nixcrypt";
+    bypassWorkqueues = true;
+  };
 
   availableKernelModules = ["nvme" "mpt3sas" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
 }
