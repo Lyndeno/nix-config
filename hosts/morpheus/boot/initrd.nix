d70d970cfe80329b158c837c90e7c6eb85032ef1
Lyndon Sanche <lsanche@lyndeno.ca>
2024-03-21 16:10:59 -0600

    host/morpheus: Allow discards on encrypted nvme

--- hosts/morpheus/boot/initrd.nix
+++ hosts/morpheus/boot/initrd.nix
@@ -4,6 +4,7 @@
   luks.devices."cryptroot" = {
     device = "/dev/disk/by-label/nixcrypt";
     bypassWorkqueues = true;
+    allowDiscards = true;
   };
 
   availableKernelModules = ["nvme" "mpt3sas" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
