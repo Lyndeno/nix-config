8dd1bf2193a294a88d998dbad7f88b2922dc144e
Lyndon Sanche <lsanche@lyndeno.ca>
2024-04-10 12:44:55 -0600

    host/morpheus: Remove mkl from paperless as fixed in NixOS/nixpkgs#299289

--- hosts/morpheus/services/paperless.nix
+++ hosts/morpheus/services/paperless.nix
@@ -1,11 +1,7 @@
 {
-  pkgs,
-  lib,
-}: {
   enable = true;
   address = "0.0.0.0";
   settings = {
     PAPERLESS_DBHOST = "/run/postgresql";
-    LD_LIBRARY_PATH = "${lib.getLib pkgs.mkl}/lib";
   };
 }
