6dc8ac626ef220cdeeaf3c970ccc92cf8025a3ac
Lyndon Sanche <lsanche@lyndeno.ca>
2024-05-15 17:02:35 -0600

    host/morpheus: Initial home-assistant config

---
+++ hosts/morpheus/services/home-assistant.nix
@@ -0,0 +1,23 @@
+{
+  enable = true;
+  extraComponents = [
+    "esphome"
+    "met"
+    "radio_browser"
+    "cast"
+    "thread"
+    "ibeacon"
+    "upnp"
+    "google_translate"
+    "tplink"
+    "plex"
+    "spotify"
+    "webostv"
+    "vesync"
+    "tuya"
+    "fitbit"
+  ];
+  config = {
+    default_config = {};
+  };
+}
