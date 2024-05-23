2ca5dcb6626b25f7840584ed55e494af58e96e1d
Lyndon Sanche <lsanche@lyndeno.ca>
2024-05-23 12:22:27 -0600

    home/lsanche: Add treesitter to vim

--- home/lsanche/programs/neovim/default.nix
+++ home/lsanche/programs/neovim/default.nix
@@ -16,50 +16,61 @@
     (lightline-vim.overrideAttrs (
       _old: let
         schemeFile = config.lib.stylix.colors inputs.base16-vim-lightline;
-        # TODO: Remove hardcoded theme file
       in {patchPhase = ''cp ${schemeFile} autoload/lightline/colorscheme/base16_${config.lib.stylix.colors.scheme-slug-underscored}.vim'';}
     ))
     nvim-lspconfig
+    nvim-treesitter.withAllGrammars
   ];
-  extraLuaConfig = ''
-    require'lspconfig'.rust_analyzer.setup{}
-    require'lspconfig'.nixd.setup{}
-  '';
+  extraLuaConfig =
+    # lua
+    ''
+      require'lspconfig'.rust_analyzer.setup{}
+      require'lspconfig'.nixd.setup{}
+
+      require'nvim-treesitter.configs'.setup {
+        highlight = {
+          enable = true,
+        },
+      }
+    '';
   # mkAfter so we can override some stylix settings
-  extraConfig = lib.mkAfter ''
-    set tabstop=2
-    set noshowmode
-    set hlsearch
-    set ignorecase
-    set incsearch
-    set cmdheight=1
-    set shiftwidth=0
-    set noshowcmd " hide the keys pressed in normal mode
-    let g:lightline = {
-      \ 'active': {
-      \   'left': [ [ 'mode', 'paste' ],
-      \             [ 'gitbranch', 'readonly', 'filename', 'modified' ] ]
-      \ },
-      \ 'component_function': {
-      \   'gitbranch': 'FugitiveHead'
-      \ },
-      \ 'colorscheme': 'base16_${config.lib.stylix.colors.scheme-slug-underscored}',
-      \ }
+  extraConfig =
+    lib.mkAfter
+    # vim
+    ''
+      set tabstop=2
+      set noshowmode
+      set hlsearch
+      set ignorecase
+      set incsearch
+      set cmdheight=1
+      set shiftwidth=0
+      set noshowcmd " hide the keys pressed in normal mode
+      let g:lightline = {
+        \ 'active': {
+        \   'left': [ [ 'mode', 'paste' ],
+        \             [ 'gitbranch', 'readonly', 'filename', 'modified' ] ]
+        \ },
+        \ 'component_function': {
+        \   'gitbranch': 'FugitiveHead'
+        \ },
+        \ 'colorscheme': 'base16_${config.lib.stylix.colors.scheme-slug-underscored}',
+        \ }
 
-    set signcolumn=number
-    set relativenumber
+      set signcolumn=number
+      set relativenumber
 
-    " To make base16 look nice
-    set termguicolors
-    set background=dark
-    let base16colorspace=256
+      " To make base16 look nice
+      set termguicolors
+      set background=dark
+      let base16colorspace=256
 
-    " To remove some of the highlights base16 introduces
-    hi Normal guibg=NONE ctermbg=NONE
-    hi NonText guibg=NONE ctermbg=NONE
-    hi CursorLineNr guibg=NONE ctermbg=NONE
-    hi LineNr guibg=NONE ctermbg=NONE
-    hi Todo cterm=none ctermfg=white ctermbg=red
+      " To remove some of the highlights base16 introduces
+      hi Normal guibg=NONE ctermbg=NONE
+      hi NonText guibg=NONE ctermbg=NONE
+      hi CursorLineNr guibg=NONE ctermbg=NONE
+      hi LineNr guibg=NONE ctermbg=NONE
+      hi Todo cterm=none ctermfg=white ctermbg=red
 
-  '';
+    '';
 }
