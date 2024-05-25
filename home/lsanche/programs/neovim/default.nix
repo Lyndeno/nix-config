1b496eb4f1d6692dc451a0a20cfe5b6dcfbc9df1
Lyndon Sanche <lsanche@lyndeno.ca>
2024-05-24 20:20:05 -0600

    home/lsanche: Add autocompletion to neovim

--- home/lsanche/programs/neovim/default.nix
+++ home/lsanche/programs/neovim/default.nix
@@ -20,14 +20,75 @@
     ))
     nvim-lspconfig
     nvim-treesitter.withAllGrammars
+    nvim-cmp
+    cmp-nvim-lsp
+    cmp_luasnip
+    luasnip
   ];
   extraLuaConfig =
     # lua
     ''
-      require'lspconfig'.rust_analyzer.setup{}
-      require'lspconfig'.nixd.setup{}
-      require'lspconfig'.clangd.setup{}
-      require'lspconfig'.texlab.setup{}
+      --require'lspconfig'.rust_analyzer.setup{}
+      --require'lspconfig'.nixd.setup{}
+      --require'lspconfig'.clangd.setup{}
+      --require'lspconfig'.texlab.setup{}
+
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local lspconfig = require('lspconfig')
+
+      local servers = { 'clangd', 'rust_analyzer', 'nixd', 'texlab' }
+      for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup {
+          on_attach = function(client, bufnr)
+            vim.lsp.inlay_hint.enable(true, {bufnr = bufnr})
+          end,
+          capabilities = capabilities,
+        }
+      end
+
+      local luasnip = require 'luasnip'
+
+      local cmp = require 'cmp'
+
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          },
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's', }),
+        }),
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        },
+      }
 
       require'nvim-treesitter.configs'.setup {
         highlight = {
