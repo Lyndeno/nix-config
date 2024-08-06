2dcb78555b9b3b4915ccbe25081d6be477dc30c2
Lyndon Sanche <lsanche@lyndeno.ca>
2024-08-06 12:10:49 -0600

    flake: Update lanzaboote

--- flake.nix
+++ flake.nix
@@ -65,7 +65,7 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.0";
+      url = "github:nix-community/lanzaboote/v0.4.1";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-parts.follows = "flake-parts";
