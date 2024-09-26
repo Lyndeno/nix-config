e3c2b46938e9ca3b4357dd27461a73bb2d6d2867
Lyndon Sanche <lsanche@lyndeno.ca>
2024-09-26 10:36:15 -0600

    host/neo: Remove redundant import

--- hosts/neo/imports.nix
+++ hosts/neo/imports.nix
@@ -3,7 +3,6 @@
   super,
 }:
 with inputs.nixos-hardware.nixosModules; [
-  common-gpu-intel-kaby-lake
   inputs.disko.nixosModules.disko
   inputs.lanzaboote.nixosModules.lanzaboote
   super.intelModule
