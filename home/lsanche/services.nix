5101756b79d1f0cb9cbfa5126f4d2935e2776f2d
Lyndon Sanche <lsanche@lyndeno.ca>
2025-02-19 10:39:45 -0700

    host/morpheus: Enable spotifyd

--- home/lsanche/services.nix
+++ home/lsanche/services.nix
@@ -1,9 +1,21 @@
-{
+{osConfig}: {
   pueue = {
     enable = true;
     settings = {
       shared = {
         default_parallel_tasks = 1;
+      };
+    };
+  };
+
+  spotifyd = {
+    enable = osConfig.networking.hostName == "morpheus";
+    settings = {
+      global = {
+        device_name = "morpheus";
+        backend = "pulseaudio";
+        bitrate = 320;
+        volume_controller = "alsa_linear";
       };
     };
   };
