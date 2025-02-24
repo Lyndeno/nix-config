97ebe28d202ecb30cc68c88388ae287deb3fe17a
Lyndon Sanche <lsanche@lyndeno.ca>
2025-02-24 14:23:56 -0700

    host/neo, mods/desktop: Move networkmanager

--- hosts/neo/default.nix
+++ hosts/neo/default.nix
@@ -1,13 +1,3 @@
 {lib}: {
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-
-  networking = {
-    networkmanager = {
-      enable = true;
-      settings.connectivity = {
-        uri = "http://google.com/generate_204";
-        response = "";
-      };
-    };
-  };
 }
