fddead1baa49ea1f377414208aa1ce56ba5efcca
Lyndon Sanche <lsanche@lyndeno.ca>
2025-02-24 12:59:29 -0700

    host/{neo, morpheus}, common: Move lanzaboote import

--- hosts/morpheus/imports.nix
+++ hosts/morpheus/imports.nix
@@ -3,5 +3,4 @@ with inputs.nixos-hardware.nixosModules; [
   common-gpu-amd
   common-cpu-amd
   common-cpu-amd-pstate
-  inputs.lanzaboote.nixosModules.lanzaboote
 ]
--- hosts/neo/imports.nix
+++ hosts/neo/imports.nix
@@ -4,7 +4,6 @@
 }:
 with inputs.nixos-hardware.nixosModules; [
   inputs.disko.nixosModules.disko
-  inputs.lanzaboote.nixosModules.lanzaboote
   super.intelModule
   common-pc-laptop-ssd
 ]
