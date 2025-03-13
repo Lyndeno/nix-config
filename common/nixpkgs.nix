0afffec2d7fad5d13dd888eb4f629f42d14545ec
Lyndon Sanche <lsanche@lyndeno.ca>
2025-03-13 14:17:54 -0600

    Update flake.lock

--- common/nixpkgs.nix
+++ common/nixpkgs.nix
@@ -8,11 +8,6 @@
           (oldAttrs.patches or [])
           ++ [
             (pkgs.fetchpatch {
-              name = "boost_is_regular.patch";
-              url = "https://github.com/astroidmail/astroid/commit/abd84171dc6c4e639f3e86649ddc7ff211077244.patch";
-              hash = "sha256-IY60AnWm18ZwrCFsOvBg76UginpMo7gXBf8GT87FqW4=";
-            })
-            (pkgs.fetchpatch {
               name = "iframe_height.patch";
               url = "https://patch-diff.githubusercontent.com/raw/astroidmail/astroid/pull/747.patch";
               hash = "sha256-7yaqIGvxvuFqKfH5htkq13hrMVuSYYKY+kWL3TWofs8=";
