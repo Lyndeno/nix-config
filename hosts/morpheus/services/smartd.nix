203465459760ce0574fd94ab33e1e9ecabd3f216
Lyndon Sanche <lsanche@lyndeno.ca>
2025-05-29 14:38:02 -0600

    host/morpheus: Schedule smart tests
    Weekly short tests
    Monthly long tests

--- hosts/morpheus/services/smartd.nix
+++ hosts/morpheus/services/smartd.nix
@@ -5,4 +5,7 @@
     recipient = "lsanche@lyndeno.ca";
     enable = true;
   };
+  # Short self test every week at 2AM
+  # Long self test every month on the 5th at 4AM
+  defaults.monitored = "-a -o on -s (S/../../7/02|L/../05/../04)";
 }
