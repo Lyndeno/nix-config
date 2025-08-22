13a0c30ff9e5cd585d73bb03dbe407bd39e0bf30
Lyndon Sanche <lsanche@lyndeno.ca>
2025-08-22 13:46:55 -0600

    host/morpheus: Re-add sleep inhibition

--- hosts/morpheus/systemd.nix
+++ hosts/morpheus/systemd.nix
@@ -2,4 +2,10 @@
   settings.Manager = {
     RuntimeWatchdogSec = "60s";
   };
+  sleep.extraConfig = ''
+    AllowSuspend=no
+    AllowHibernation=no
+    AllowSuspendThenHibernate=no
+    AllowHybridSleep=no
+  '';
 }
