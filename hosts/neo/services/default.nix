58f2e2d55a6c95ff110b1141f1044694349cf0e0
Lyndon Sanche <lsanche@lyndeno.ca>
2025-08-25 00:04:32 -0700

    host/neo: Enable automatic timezone

--- hosts/neo/services/default.nix
+++ hosts/neo/services/default.nix
@@ -1,4 +1,6 @@
 {
   upower.criticalPowerAction = "Hibernate";
   tailscale.useRoutingFeatures = "client";
+  automatic-timezoned.enable = true;
+  geoclue2.geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
 }
