6c06f80d214cc99a02ef96ab84ff3cc3905c52a1
Lyndon Sanche <lsanche@lyndeno.ca>
2025-09-04 11:11:17 +0700

    home/lsanche: Temporarily disable systemd lsp

--- home/lsanche/programs/nixvim.nix
+++ home/lsanche/programs/nixvim.nix
@@ -112,7 +112,7 @@ in {
       bashls.enable = true;
       gitlab_ci_ls.enable = true;
       ruff.enable = true;
-      systemd_ls.enable = true;
+      #systemd_ls.enable = true;
       gopls.enable = true;
       neocmake.enable = true;
       just.enable = true;
