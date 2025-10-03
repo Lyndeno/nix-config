23662edac26868499887a6806f60b87368190147
Lyndon Sanche <lsanche@lyndeno.ca>
2025-10-03 11:35:18 -0600

    host/morpheus: Add features for native compilation

--- hosts/morpheus/default.nix
+++ hosts/morpheus/default.nix
@@ -1,19 +1,31 @@
 {
   # Set your time zone.
   time.timeZone = "America/Edmonton";
-  nix.buildMachines = [
-    {
-      hostName = "localhost";
-      protocol = null;
-      systems = ["x86_64-linux" "aarch64-linux"];
-      supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
-      maxJobs = 32;
-    }
-  ];
-  nix.settings.allowed-uris = [
-    "github:"
-    "git+https://github.com/"
-    "git+ssh://github.com/"
-    "https://devimages-cdn.apple.com/"
-  ];
+  nix = {
+    buildMachines = [
+      {
+        hostName = "localhost";
+        protocol = null;
+        systems = ["x86_64-linux" "aarch64-linux"];
+        supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark" "gccarch-znver3" "gccarch-skylake"];
+        maxJobs = 32;
+      }
+    ];
+    settings = {
+      allowed-uris = [
+        "github:"
+        "git+https://github.com/"
+        "git+ssh://github.com/"
+        "https://devimages-cdn.apple.com/"
+      ];
+      system-features = [
+        "kvm"
+        "nixos-test"
+        "big-parallel"
+        "benchmark"
+        "gccarch-znver3"
+        "gccarch-skylake"
+      ];
+    };
+  };
 }
