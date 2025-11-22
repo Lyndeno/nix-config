ad1f43b4bd533b60b0616b493cd95b00de1cd50f
Lyndon Sanche <lsanche@lyndeno.ca>
2025-11-22 08:47:18 -0700

    modules: Move virtualisation

---
+++ modules/nixos/virtualisation/default.nix
@@ -0,0 +1,23 @@
+{
+  pkgs,
+  lib,
+}: {
+  programs.dconf.enable = lib.mkDefault true;
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    docker-compose
+  ];
+
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+
+      qemu = {
+        package = pkgs.qemu_kvm;
+        swtpm.enable = true;
+      };
+    };
+
+    docker.enable = true;
+  };
+}
