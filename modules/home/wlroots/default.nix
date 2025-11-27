23e2f0ebe19c2e54129154c15cb5a03a12e66063
Lyndon Sanche <lsanche@lyndeno.ca>
2025-11-27 12:47:50 -0700

    modules/home: Move alacritty to own module

--- modules/home/wlroots/default.nix
+++ modules/home/wlroots/default.nix
@@ -24,29 +24,6 @@
   ];
 
   programs = {
-    alacritty = {
-      enable = true;
-      # colours and font managed by stylix
-      settings = {
-        window = {
-          padding = {
-            x = 12;
-            y = 12;
-          };
-          dynamic_padding = true;
-          decorations = "None";
-        };
-        mouse.hide_when_typing = true;
-        keyboard.bindings = [
-          {
-            key = "Space";
-            mods = "Control";
-            chars = "\\u0000";
-          }
-        ];
-      };
-    };
-
     waybar = let
       inherit (osConfig.networking) hostName;
       fanQuery =
