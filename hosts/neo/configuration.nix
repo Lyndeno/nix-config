a98f1a671c5b7f9eda85942750bfc0a19244f942
Lyndon Sanche <lsanche@lyndeno.ca>
2025-11-27 21:10:17 -0700

    modules/nixos: Move some laptop specific items to laptop module

--- hosts/neo/configuration.nix
+++ hosts/neo/configuration.nix
@@ -94,10 +94,7 @@
   };
 
   services = {
-    upower.criticalPowerAction = "Hibernate";
     tailscale.useRoutingFeatures = "client";
-    automatic-timezoned.enable = true;
-    geoclue2.geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
   };
 
   systemd = {
--- modules/nixos/laptop/default.nix
+++ modules/nixos/laptop/default.nix
@@ -4,7 +4,10 @@
   '';
 
   services = {
+    upower.criticalPowerAction = "Hibernate";
     logind.settings.Login.HandleLidSwitch = "suspend-then-hibernate";
+    automatic-timezoned.enable = true;
+    geoclue2.geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
   };
 
   environment.systemPackages = with pkgs; [
