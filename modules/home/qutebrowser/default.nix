5b58ac6a28ced7e57f0bbb547e153dccf8ce6827
Lyndon Sanche <lsanche@lyndeno.ca>
2025-11-27 18:07:39 -0700

    modules/home: Move qutebrowser

---
+++ modules/home/qutebrowser/default.nix
@@ -0,0 +1,9 @@
+{
+  programs.qutebrowser = {
+    enable = true;
+    settings = {
+      window.hide_decoration = true;
+      tabs.show = "multiple";
+    };
+  };
+}
--- old/home/lsanche/programs/qutebrowser.nix
+++
@@ -1,7 +0,0 @@
-{
-  #inherit (osConfig.modules.desktop) enable;
-  settings = {
-    window.hide_decoration = true;
-    tabs.show = "multiple";
-  };
-}
