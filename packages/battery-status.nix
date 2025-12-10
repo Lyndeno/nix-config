1c6e6ea4a7f3f0311475ef92573cee0f28ca2497
Lyndon Sanche <lsanche@lyndeno.ca>
2025-12-09 21:01:51 -0700

    packages: Make battery-status more robust and add tests

--- packages/battery-status.nix
+++ packages/battery-status.nix
@@ -1,16 +1,82 @@
 {
   pkgs,
   pname,
-}:
-pkgs.writeShellApplication {
-  name = pname;
+}: let
+  package = pkgs.writeShellApplication {
+    name = pname;
 
-  runtimeInputs = with pkgs; [
-    acpi
-    coreutils
-  ];
+    runtimeInputs = with pkgs; [
+      acpi
+      coreutils
+    ];
 
-  text = ''
-    acpi -a | cut -d" " -f3 | cut -d- -f1
-  '';
-}
+    text = ''
+      STATUS=$(acpi -a | cut -d" " -f3 | cut -d- -f1)
+      if [ "$STATUS" = "off" ] || [ "$STATUS" = "on" ]
+      then
+        echo "$STATUS"
+      else
+        echo "n/a"
+      fi
+    '';
+
+    passthru.tests = {
+      plugged-in = pkgs.testers.nixosTest {
+        name = "battery-status-plugged-in";
+
+        nodes.machine = _: {
+          environment.systemPackages = [package];
+          system.stateVersion = "25.11";
+          boot.kernelModules = ["test_power"];
+          boot.kernelParams = ["test_power.ac_online=on"];
+        };
+
+        testScript = ''
+          import sys
+          output = machine.execute("battery-status")
+          if (output[1].strip() == "on"):
+            sys.exit(0)
+          else:
+            sys.exit(-1)
+        '';
+      };
+      unplugged = pkgs.testers.nixosTest {
+        name = "battery-status-unplugged";
+
+        nodes.machine = _: {
+          environment.systemPackages = [package];
+          system.stateVersion = "25.11";
+          boot.kernelModules = ["test_power"];
+          boot.kernelParams = ["test_power.ac_online=off"];
+        };
+
+        testScript = ''
+          import sys
+          output = machine.execute("battery-status")
+          if (output[1].strip() == "off"):
+            sys.exit(0)
+          else:
+            sys.exit(-1)
+        '';
+      };
+      no-battery = pkgs.testers.nixosTest {
+        name = "battery-status-no-battery";
+
+        nodes.machine = _: {
+          environment.systemPackages = [package];
+          system.stateVersion = "25.11";
+        };
+
+        testScript = ''
+          import sys
+          output = machine.execute("battery-status")
+          if (output[1].strip() == "n/a"):
+            sys.exit(0)
+          else:
+            sys.exit(-1)
+        '';
+      };
+    };
+  };
+in
+  package
