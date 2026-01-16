75de3342344d7efb2244b9f15239017c623d97cf
Lyndon Sanche <lsanche@lyndeno.ca>
2026-01-15 19:28:44 -0700

    Update flake.lock

--- hosts/morpheus/configuration.nix
+++ hosts/morpheus/configuration.nix
@@ -236,7 +236,7 @@
       };
     };
     hydra = {
-      enable = true;
+      #enable = true;
       hydraURL = "https://hydra.lyndeno.ca";
       notificationSender = "hydra@morpheus";
       useSubstitutes = true;
