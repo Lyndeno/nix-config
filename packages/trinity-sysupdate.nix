ee1a8713aac83bf00cd10e8fa00358dcdae8ba32
Lyndon Sanche <lsanche@lyndeno.ca>
2026-01-15 19:35:27 -0700

    Make trinity images x86 only
    repart is not working under binfmt right now

--- packages/trinity-sysupdate.nix
+++ packages/trinity-sysupdate.nix
@@ -1,18 +1,15 @@
 {
   flake,
   system,
-  pkgs,
   ...
-}: let
-  inherit (pkgs) lib;
-in
-  (flake.nixosConfigurations.trinity.extendModules {
-    modules = [
-      {
-        nixpkgs.buildPlatform = system;
-        nixpkgs.hostPlatform = "aarch64-linux";
-      }
-    ];
-  }).config.system.build.sysupdate-package.overrideAttrs (_: _: {
-    meta.platforms = lib.platforms.linux;
-  })
+}:
+(flake.nixosConfigurations.trinity.extendModules {
+  modules = [
+    {
+      nixpkgs.buildPlatform = system;
+      nixpkgs.hostPlatform = "aarch64-linux";
+    }
+  ];
+}).config.system.build.sysupdate-package.overrideAttrs (_: _: {
+  meta.platforms = ["x86_64-linux"];
+})
