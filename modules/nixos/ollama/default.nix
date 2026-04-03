716cf05a031aa091259a276d3736cfa7ff7726ad
Lyndon Sanche <lsanche@lyndeno.ca>
2026-04-03 13:01:20 -0600

    modules/ollama: Allow access through proxy

--- modules/nixos/ollama/default.nix
+++ modules/nixos/ollama/default.nix
@@ -8,11 +8,13 @@
       enable = true;
       package = pkgs.ollama-rocm;
       rocmOverrideGfx = "10.3.0";
+      host = "0.0.0.0";
     };
 
     open-webui = {
       enable = true;
       port = 8082;
+      host = "0.0.0.0";
       environment = {
         OLLAMA_API_BASE_URL = "http://127.0.0.1:${toString config.services.ollama.port}";
         WEBUI_AUTH = "False";
