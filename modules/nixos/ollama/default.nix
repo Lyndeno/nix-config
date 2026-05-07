a75c47f71d3f951818d98100f1147226e9bc8d54
Lyndon Sanche <lsanche@lyndeno.ca>
2026-05-07 06:41:54 -0600

    chore: Disable open-webui

--- modules/nixos/ollama/default.nix
+++ modules/nixos/ollama/default.nix
@@ -1,8 +1,4 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   services = {
     ollama = {
       enable = true;
@@ -14,22 +10,22 @@
       };
     };
 
-    open-webui = {
-      enable = true;
-      port = 8082;
-      host = "0.0.0.0";
-      environment = {
-        OLLAMA_API_BASE_URL = "http://127.0.0.1:${toString config.services.ollama.port}";
-        WEBUI_AUTH = "False";
-      };
-    };
+    #open-webui = {
+    #  enable = true;
+    #  port = 8082;
+    #  host = "0.0.0.0";
+    #  environment = {
+    #    OLLAMA_API_BASE_URL = "http://127.0.0.1:${toString config.services.ollama.port}";
+    #    WEBUI_AUTH = "False";
+    #  };
+    #};
 
     localProxy.subDomains = {
       ollama = {};
-      ai = {
-        inherit (config.services.open-webui) port;
-        extraConfig.proxyWebsockets = true;
-      };
+      #ai = {
+      #  inherit (config.services.open-webui) port;
+      #  extraConfig.proxyWebsockets = true;
+      #};
     };
   };
 }
