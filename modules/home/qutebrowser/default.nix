4b74812476309d302d0f4fa781d777a6f3bb2993
Lyndon Sanche <lsanche@lyndeno.ca>
2026-05-12 13:20:37 -0600

    feat: Add some sane qutebrowser settings

--- modules/home/qutebrowser/default.nix
+++ modules/home/qutebrowser/default.nix
@@ -1,11 +1,22 @@
-{
+{lib, ...}: {
   programs.qutebrowser = {
     enable = true;
     settings = {
       window.hide_decoration = true;
+      statusbar.show = "in-mode";
+      scrolling.bar = "never";
       tabs = {
         show = "switching";
         position = "left";
+      };
+    };
+    keyBindings = {
+      normal = {
+        "<Ctrl-e>" = lib.mkMerge [
+          "config-cycle tabs.show switching always"
+          "config-cycle statusbar.show in-mode always"
+          "config-cycle scrolling.bar never overlay"
+        ];
       };
     };
   };
